b215c786b5b0b682343ed2dc74929f91c518d38d
LeeAFH <footehud@gmail.com>
2017-07-08 14:18:39 -0400

    saved the same layout from meka for IDS etc. testign purposes

---
+++ javascript.js
@@ -0,0 +1,52 @@
+//Yelp Boys Night and Random Events and Outdoor/festival? 
+//
+//
+//GENERAL FLOW
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//VARIBALES 
+//YELP
+var yelpapiKey =
+var yelpqueryURL = "https://api.yelp.com/v3/businesses/search"
+// var searchQuery = 
+//
+//
+//calling the search API from YELP       
+    
+    $.ajax({
+        url: (yelpqueryURL + searchQuery + yelpapiKey),
+        method: "GET"
+    }).done(function(response) {
+	    //see what matches up 
+	    console.log(response);
+//
+//
+//
+//RESOURCES
+//
+//firebase
+////
+//ajax call 
+//
+//Key for Facebook events 
+//
+//
+//
+//
+//
+//Key Yelp 
+//
+//Client ID
+// ImyIU6DHaqlzfq2Y-v7UPw
+// Client Secret
+// SOC31MI8AVBkGCnk6At0ScKs8qxdhl3CWtDfX7BF1OoTgSPBUbGONwhNb1i8Ozy1
+//
+//
+//
